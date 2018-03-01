5d6ba81576656ecdb37891f438f384de4a05fda3
MonikaBurek <monika.burek.programistka@gmail.com>
2018-03-01 22:01:14 +0100

    klasa uzytkownik

--- main.cpp
+++ main.cpp
@@ -1,6 +1,65 @@
 #include <iostream>
 
 using namespace std;
+/*
+Klasa na podstawie, ktorej jest tworzony obiekt uzytkownik (z prywatnymipolamioraz publicznymi getterami i setterami).
+*/
+class Uzytkownik
+{
+    int idUzytkownika;
+    string nazwa;
+    string haslo;
+
+ public:
+
+// Konstruktor domyslny
+    Uzytkownik()
+    {
+        idUzytkownika = null;
+        nazwa = null;
+        haslo = null;
+    }
+
+// Konstruktor inicjiujacy atrybuty
+    Uzytkownik(int numberID, string name, string password)
+    {
+        idUzytkownika = numberID;
+        nazwa = name;
+        haslo = password;
+    }
+
+ //Ustawia numer ID uzytkownika
+ void setIdUzytkownika(int numberID)
+ {
+    idUzytkownika = numberID;
+ }
+
+ //Zwraca numer ID uzytkownika
+ int getIdUzytkownika()
+ {
+     return idUzytkownika;
+ }
+
+ void setNazwaUzytkownika(string name)
+ {
+    nazwa = name;
+ }
+
+string getNazwaUzytkownika()
+ {
+     return nazwa;
+ }
+
+ void setHasloUzytkownika(string password)
+ {
+    haslo = password;
+ }
+
+string getHasloUzytkownika()
+ {
+     return haslo;
+ }
+
 
 int main()
 {
